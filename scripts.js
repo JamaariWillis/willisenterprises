18541efb828ed8e0a6113a01d4b1274f7c832573
Jamaari <jamaari1w@gmail.com>
2024-10-17 08:55:15 -0400

    upda

--- scripts.js
+++ scripts.js
@@ -7,25 +7,26 @@ window.addEventListener('scroll', function() {
 
         setTimeout(() => {
             if (window.scrollY > 100) {
-                topBar.style.display = 'block';
+                topBar.classList.remove('hidden');
                 setTimeout(() => {
                     topBar.style.opacity = 1;
-                }, 10); // Small timeout to ensure display change takes effect
+                }, 10);
             }
-        }, 300); // Match this duration with the CSS transition duration
+        }, 300);
     } else {
         topBar.style.opacity = 0;
 
         setTimeout(() => {
             if (window.scrollY <= 100) {
-                topBar.style.display = 'none';
+                topBar.classList.add('hidden');
                 bottomBar.style.opacity = 1;
             }
-        }, 300); // Match this duration with the CSS transition duration
+        }, 300);
     }
 });
 
 // Initialize top bar opacity to 0 and display to none
 const topBarInit = document.querySelector('.top-bar');
 topBarInit.style.opacity = 0;
-topBarInit.style.display = 'none';
+topBarInit.classList.add('hidden');
+
